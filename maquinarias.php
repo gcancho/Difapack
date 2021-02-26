33378651b21e0c981fa0941897de78e0f3afb420
gcancho <giovannicb19@gmail.com>
2021-02-26 18:39:05 -0500

    vista servicios mobile

---
+++ maquinarias.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "head.php" ?>
+
+<body>
+
+<?php include "header.php" ?>
+
+<section class="maquinas maquinas--margin">
+    <div class="maquinas__wrap wrap">
+        <div class="line"></div>
+        <h2 class="maquinas__titulo maquinas__titulo--display titulo">NUESTRAS MÁQUINAS</h2>
+        <div class="maquinas__lista">
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">Máquinas</label>
+                <label class="maquinas__nombre">Llenadoras de botellas</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquina_llenadoras_de_botellas.jpg" alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">MÁQUINAS</label>
+                <label class="maquinas__nombre">Dosificadoras con tolva</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquinas_dosificadora_con_tolva.jpg" alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">MÁQUINAS</label>
+                <label class="maquinas__nombre">Dosificadoras de miel</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquina_dosificadora_de_miel.jpg" alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">MÁQUINAS</label>
+                <label class="maquinas__nombre">Dosificadoras sistema aguja</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquinas_dosificadora_sistema_aguja.jpg"
+                    alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">MÁQUINAS</label>
+                <label class="maquinas__nombre">Enjuagadoras</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquinas_enjuagadoras_de_botellas_o_potes.jpg"
+                    alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">MÁQUINAS</label>
+                <label class="maquinas__nombre">Roscadoras</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquinas_roscadoras.jpg" alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+            <article class="maquinas__card">
+                <label class="maquinas__nombre">MÁQUINAS</label>
+                <label class="maquinas__nombre">Llenadoras de pintura</label>
+                <img class="maquinas__foto" src="./imagenes/maquinas/Maquinas_llenadoras_de_pintura.jpg" alt="">
+                <a href="#" class="maquinas__boton boton">VER MÁS</a>
+            </article>
+        </div>
+    </div>
+</section>
+
+<?php include "footer.php" ?>
+
+<?php include "scripts.php" ?>
+
+</body>
+</html>
---
+++ servicios.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "head.php" ?>
+
+<body>
+    
+<?php include "header.php" ?>
+
+<section class="nosotros">
+   <div class="nosotros__wrap wrap">
+       <h2 class="nosotros__titulo titulo">NUESTROS SERVICIOS</h2>
+       <div class="nosotros__contenido">
+           <img class="nosotros__imagen" src="./imagenes/maquinas/Maquina_llenadoras_de_botellas.jpg" alt="">
+           <div class="servicios">
+                <div class="servicios__item">
+                    <i class="fas fa-check-square"></i>
+                    <p class="servicios__parrafo">Mantenimiento preventivo y correctivo de todo tipo de maquinas envasadoras y empacadoras</p>
+                </div>
+                <div class="servicios__item">
+                    <i class="fas fa-check-square"></i>
+                    <p class="servicios__parrafo">Suministro de partes y repuestos</p>
+                </div>
+                <div class="servicios__item">
+                    <i class="fas fa-check-square"></i>
+                    <p class="servicios__parrafo">Fabricaciones especiales en acero inoxidable</p>
+                </div>
+                <div class="servicios__item">
+                    <i class="fas fa-check-square"></i>
+                    <p class="servicios__parrafo">Servicio metalmecánico en general</p>
+                </div>
+                <div class="servicios__item">
+                    <i class="fas fa-check-square"></i>
+                    <p class="servicios__parrafo">Fabricación de transportadores de botellas, cajas y otros</p>
+                </div>
+                <div class="servicios__item">
+                    <i class="fas fa-check-square"></i>
+                    <p class="servicios__parrafo">Fabricación de unidades modulares de envasado</p>
+                </div>
+           </div>
+       </div>
+       <img src="" alt="">
+   </div>  
+</section>
+
+<?php include "footer.php" ?>
+
+<?php include "scripts.php" ?>
+
+</body>
+</html>
