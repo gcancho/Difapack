53da9906a50986ca211eb6eb15aad78fa498581d
gcancho <giovannicb19@gmail.com>
2021-03-12 00:14:41 -0500

    Vista videos con sus respectivos nombres

--- videos.php
+++ videos.php
@@ -5,71 +5,124 @@
 
 <body>
     <?php include "includes/header.php" ?>
-    
-    <section class="videos">
-        <div class="videos__wrap wrap">
-            <h2 class="videos__titulo titulo">Videos</h2>
-            <div class="videos__lista">
-                <article class="videos__card">
-                    <h3 class="videos__subtitulo">
-                        <span>Máquina</span> dosificadora de alcohol o vainilla
-                    </h3>
-                    <div class="videos__video">
-                        <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </div>
-                </article>
-                <article class="videos__card">
-                    <h3 class="videos__subtitulo">
-                        <span>Máquina</span> dosificadora de alcohol o vainilla
-                    </h3>
-                    <div class="videos__video">
-                        <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </div>
-                </article>
-                <article class="videos__card">
-                    <h3 class="videos__subtitulo">
-                        <span>Máquina</span> dosificadora de alcohol o vainilla
-                    </h3>
-                    <div class="videos__video">
-                        <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </div>
-                </article>
-                <article class="videos__card">
-                    <h3 class="videos__subtitulo">
-                        <span>Máquina</span> dosificadora de alcohol o vainilla
-                    </h3>
-                    <div class="videos__video">
-                        <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </div>
-                </article>
-                <article class="videos__card">
-                    <h3 class="videos__subtitulo">
-                        <span>Máquina</span> dosificadora de alcohol o vainilla
-                    </h3>
-                    <div class="videos__video">
-                        <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </div>
-                </article>
-                <article class="videos__card">
-                    <h3 class="videos__subtitulo">
-                        <span>Máquina</span> dosificadora de alcohol o vainilla
-                    </h3>
-                    <div class="videos__video">
-                        <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </div>
-                </article>
+
+    <div class="content">
+        <section class="videos">
+            <div class="videos__wrap wrap">
+                <h2 class="videos__titulo titulo">Videos</h2>
+                <div class="videos__lista">
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> dosificadora de alcohol o vainilla
+                        </h3>
+                        <div class="videos__video">
+                            <iframe src="https://www.youtube.com/embed/gsDFax_OwGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> dosificadora de nectar
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/2BIlB_qaPCE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> envasadora de alcohol
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/vP-3UEJE5Hg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> dosificadora de extractos
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/vbrNEP4dFjk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> envasadora de manjar (Rellenos)
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/5RRy7u66iw4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> llenadora de kekes
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/gxcRXZVPKqc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> depositadora de jarabes
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/bOHfwIEXFfs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> llenadora de extractos
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/NgJMi08gBYQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> dosificadora de enjuague bucal
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/f_adSRZTiso" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> envasadora de pintura
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/v8Xg9WefRdU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> dosificadora de kekes gran capacidad
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/-dDxvai4USQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Máquina</span> envasadora de agua en bidones de 20 L
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/x1939CTFVDQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                    <article class="videos__card">
+                        <h3 class="videos__subtitulo">
+                            <span>Picos</span> para máquinas dosificadoras de botellas, galoneras, bidones, garrafones, etc.
+                        </h3>
+                        <div class="videos__video">
+                            <iframe width="440" height="278" src="https://www.youtube.com/embed/GEYtyX_JA9c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </article>
+                </div>
             </div>
-        </div>
-    </section>
-    
+        </section>
+    </div>
+
     <?php include "includes/footer.php" ?>
     <?php include "includes/scripts.php" ?>
-    
+
 </body>
+
 </html>
